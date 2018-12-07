504bdec80e5f477736599b62fdf8499bab41c493
tiaaristianti <tiaaristianti@gmail.com>
2018-12-07 18:10:31 +0700

    kalkulator

---
+++ assets/js/javascript.js
@@ -0,0 +1,20 @@
+function tambah(){
+  val = document.getElementsByName('nilai')[0].value;
+  document.getElementsByName('nilai')[0].value += "+";
+}
+function kurang(){
+  val = document.getElementsByName('nilai')[0].value;
+  document.getElementsByName('nilai')[0].value += "-";
+}
+function kali(){
+  val = document.getElementsByName('nilai')[0].value;
+  document.getElementsByName('nilai')[0].value += "*";
+}
+function bagi(){
+  val = document.getElementsByName('nilai')[0].value;
+  document.getElementsByName('nilai')[0].value += "/";
+}
+function hitung(){
+  val = document.getElementsByName('nilai')[0].value;
+  document.getElementsByName('hasil')[0].value = eval(val);
+}
